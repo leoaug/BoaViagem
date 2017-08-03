626b87e0af7aa5ef77fddafb02f76f7a05a52a5e
c1260311 <c1260311@ESBSA019941LA1.intrabb.bb.com.br>
2017-08-03 15:43:28 -0300

    activity

---
+++ src/br/com/casadocodigo/boaviagem/activity/GastoActivity.java
@@ -0,0 +1,15 @@
+package br.com.casadocodigo.boaviagem.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import br.com.casadocodigo.boaviagem.R;
+
+public class GastoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gasto);
+				
+	}
+}
--- src/br/com/casadocodigo/boaviagem/activity/ViagemActivity.java
+++ src/br/com/casadocodigo/boaviagem/activity/ViagemActivity.java
@@ -1,26 +1,27 @@
 package br.com.casadocodigo.boaviagem.activity;
 
-import java.util.Calendar;
+
 
 import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.DatePickerDialog.OnDateSetListener;
-import android.app.Dialog;
+
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import br.com.casadocodigo.boaviagem.R;
-import br.com.casadocodigo.boaviagem.R.id;
-import br.com.casadocodigo.boaviagem.R.layout;
+import br.com.casadocodigo.boaviagem.constantes.ConstantesFragment;
+import br.com.casadocodigo.boaviagem.fragment.DatePickerFragment;
 
 public class ViagemActivity extends Activity {
 
-	private int ano, mes, dia;
 	
 	private Button dataChegada;
+	private DatePicker pickerDataChegada;
+	
 	private Button dataSaida;
+	private DatePicker pickerDataSaida;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,41 +35,22 @@ public class ViagemActivity extends Activity {
 		dataSaida.setText("Selecione");
 	}
 	
-
-	private OnDateSetListener listener = new OnDateSetListener() {
-		
-		@Override
-		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-			ano = year;
-			mes = monthOfYear;
-			dia = dayOfMonth;
-		
-			dataChegada.setText(dia + "/" + (mes+1) + "/" + ano);
-			dataSaida.setText(dia + "/" + (mes+1) + "/" + ano);
-		}
-	};
-
-	
-	@Override
-	protected Dialog onCreateDialog(int id) {
-	
-		if(R.id.dataChegada == id || R.id.dataSaida == id) {
-			return new DatePickerDialog(this, listener, ano, mes, dia);
-		}
-	
-		return null;
-	}
 	
 	public void selecionarData(View view){
-		showDialog(view.getId());
+
+		DialogFragment newFragment = new DatePickerFragment(dataChegada,dataSaida);
+		if(view.getId() == R.id.dataChegada) {
+			newFragment.show(getFragmentManager(), ConstantesFragment.DIALOG_FRAGMENT_DATA_INICIO.getDialogFragment());
+		} else {
+			newFragment.show(getFragmentManager(), ConstantesFragment.DIALOG_FRAGMENT_DATA_FIM.getDialogFragment());
+		}
 		
 	}
 	
 	/**
 	 * 
 	 * @param view metodo para voltar para o dashboard
-	 */
-	
+	 */	
 	public void salvarViagem(View view) {
 		
 	}
